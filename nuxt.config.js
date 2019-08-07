e9dac72546ff53fcc11e0341b0d34a90141d5b98
hyc <992273994@qq.com>
2019-08-07 16:58:14 +0800

    spanner

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,76 +1,72 @@
-const colors = require('vuetify/es5/util/colors').default
+const colors = require('vuetify/es5/util/colors').default;
 
 module.exports = {
-  mode: 'universal',
-  /*
+	mode: 'universal',
+	/*
   ** Headers of the page
   */
-  head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
+	server: {
+		port: 8009, // default: 3000
+		host: '0.0.0.0' // default: localhost
+	},
+	head: {
+		titleTemplate: '%s - ' + process.env.npm_package_name,
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+		],
+		link: [ { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' } ]
+	},
+	/*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
-  /*
+	loading: { color: '#fff' },
+	/*
   ** Global CSS
   */
-  css: [
-  ],
-  /*
+	css: [],
+	/*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
-  /*
+	plugins: [],
+	/*
   ** Nuxt.js dev-modules
   */
-  devModules: [
-    '@nuxtjs/vuetify',
-  ],
-  /*
+	devModules: [ '@nuxtjs/vuetify' ],
+	/*
   ** Nuxt.js modules
   */
-  modules: [
-  ],
-  /*
+	modules: [],
+	/*
   ** vuetify module configuration
   ** https://github.com/nuxt-community/vuetify-module
   */
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
-  },
-  /*
+	vuetify: {
+		customVariables: [ '~/assets/variables.scss' ],
+		theme: {
+			dark: true,
+			themes: {
+				dark: {
+					primary: colors.blue.darken2,
+					accent: colors.grey.darken3,
+					secondary: colors.amber.darken3,
+					info: colors.teal.lighten1,
+					warning: colors.amber.base,
+					error: colors.deepOrange.accent4,
+					success: colors.green.accent3
+				}
+			}
+		}
+	},
+	/*
   ** Build configuration
   */
-  build: {
-    /*
+	build: {
+		/*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
-    }
-  }
-}
+		extend(config, ctx) {}
+	}
+};
